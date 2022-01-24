28758b533ddce4d1dead38224eb63121e69bbf69
haslzun16 <47390845+haslzun16@users.noreply.github.com>
2022-01-24 14:30:05 -0500

    added hide show Password

--- screens/SignInScreen.tsx
+++ screens/SignInScreen.tsx
@@ -1,11 +1,25 @@
 import * as React from "react";
-import { View, ViewStyle, TextStyle, Alert } from "react-native";
+import {
+  View,
+  ViewStyle,
+  TextStyle,
+  Alert,
+  TouchableOpacity,
+} from "react-native";
 import { color, spacing, typography } from "../theme";
-import { Button, Screen, Text, Header, TextField } from "../components";
+import {
+  Button,
+  Screen,
+  Text,
+  Header,
+  TextField,
+  AutoImage as Image,
+} from "../components";
 
 import { useNavigation } from "@react-navigation/native";
 import { useAuth } from "../contexts/auth";
 import { useToast } from "../components/Toast";
+const eye = require("../assets/images/reveal.png");
 
 // Styles
 const CONTAINER: ViewStyle = {
@@ -27,12 +41,13 @@ export default function SignInScreen() {
 
   const [email, setEmail] = React.useState("");
   const [password, setPassword] = React.useState("");
+  const [reveal, setReveal] = React.useState<boolean>(true);
   const { signIn } = useAuth();
 
   const onLoginPressed = () => {
     //Generate User Token Server-Side
     if (email === "" || password === "") {
-      toast.show("You must provide an email and password", {color: 'red'});
+      toast.show("You must provide an email and password", { color: "red" });
     } else {
       signIn(email, password);
     }
@@ -40,10 +55,7 @@ export default function SignInScreen() {
 
   return (
     <Screen style={CONTAINER}>
-      <Header
-        leftIcon="back"
-        onLeftPress={() => navigation.goBack()}
-      />
+      <Header leftIcon="back" onLeftPress={() => navigation.goBack()} />
 
       <View style={HEADER}>
         <Text preset="header" text="Sign in to POPN" />
@@ -67,20 +79,49 @@ export default function SignInScreen() {
           }}
         />
 
-        <TextField
-          onChangeText={(value) => setPassword(value)}
-          value={password}
-          label="Password"
-          placeholder="Enter your password"
-          secureTextEntry
-          inputStyle={{
-            padding: 16,
-            borderWidth: 2,
-            borderRadius: 4,
-            borderColor: "black",
-            marginTop: 6,
+        <View
+          style={{
+            display: "flex",
+            flexDirection: "row",
+            width: "100%",
           }}
-        />
+        >
+          <TextField
+            onChangeText={(value) => setPassword(value)}
+            value={password}
+            label="Password"
+            placeholder="Enter your password"
+            secureTextEntry={reveal}
+            inputStyle={{
+              padding: 16,
+              borderWidth: 2,
+              borderRadius: 4,
+              borderColor: "black",
+              marginTop: 6,
+            }}
+            style={{ width: "100%" }}
+          />
+          <TouchableOpacity
+            style={{
+              alignItems: "center",
+              justifyContent: "center",
+              margin: "auto",
+            }}
+            onPress={() => setReveal((prev) => !prev)}
+          >
+            <Image
+              source={eye}
+              style={[
+                {
+                  marginLeft: 10,
+                  justifyContent: "center",
+                  marginTop: 15,
+                },
+                reveal == true ? { opacity: 0.3 } : null,
+              ]}
+            />
+          </TouchableOpacity>
+        </View>
         <View style={{ flexDirection: "row", justifyContent: "flex-end" }}>
           <Button
             style={{ marginLeft: 5 }}
