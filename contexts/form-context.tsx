c36964ee8f8fa1c6c02d56c48556c6d1935031e9
haslzun16 <47390845+haslzun16@users.noreply.github.com>
2022-03-23 14:01:18 -0400

    implemented the auth provider

--- contexts/form-context.tsx
+++ contexts/form-context.tsx
@@ -1,83 +1,88 @@
-import * as React from "react";
+import * as React from 'react';
 
 const FormStateContext = React.createContext();
 const FormDispatchContext = React.createContext();
 
 const initialState = {
-    user: {
-        values: {},
-        errors: {}
-    }
+	user: {
+		values: {},
+		errors: {},
+	},
 };
 
 const formReducer = (state: any, action: any) => {
-    const { type, payload } = action;
-    switch (type) {
-        case "SET_FORM":
-            return {
-                ...state,
-                [payload.id]: payload.data
-            };
-        case "UPDATE_FORM":
-            return {
-                ...state,
-                [payload.id]: {
-                    ...payload.data
-                }
-            };
-        case "UPDATE_VALUES":
-            return {
-                ...state,
-                [payload.id]: {
-                    values: {
-                        ...payload.data
-                    }
-                }
-            };
-        case "UPDATE_ERRORS":
-            return {
-                ...state,
-                [payload.id]: {
-                    errors: {
-                        ...payload.data
-                    }
-                }
-            };
-        default:
-            return state;
-    }
+	const { type, payload } = action;
+	switch (type) {
+		case 'SET_FORM':
+			return {
+				...state,
+				[payload.id]: payload.data,
+			};
+		case 'UPDATE_FORM':
+			return {
+				...state,
+				[payload.id]: {
+					...payload.data,
+				},
+			};
+		case 'UPDATE_VALUES':
+			return {
+				...state,
+				[payload.id]: {
+					values: {
+						...payload.data,
+					},
+				},
+			};
+		case 'UPDATE_ERRORS':
+			return {
+				...state,
+				[payload.id]: {
+					errors: {
+						...payload.data,
+					},
+				},
+			};
+		case 'RESET_FORM':
+			return {
+				...state,
+				[payload.id]: payload.data,
+			};
+		default:
+			return state;
+	}
 };
 
 const FormProvider = ({ children }) => {
-    const [state, dispatch] = React.useReducer(formReducer, initialState);
+	const [state, dispatch] = React.useReducer(formReducer, initialState);
 
-    return (
-        <FormStateContext.Provider value={state}>
-            <FormDispatchContext.Provider value={dispatch}>
-                {children}
-            </FormDispatchContext.Provider>
-        </FormStateContext.Provider>
-    );
+	return (
+		<FormStateContext.Provider value={state}>
+			<FormDispatchContext.Provider value={dispatch}>
+				{children}
+			</FormDispatchContext.Provider>
+		</FormStateContext.Provider>
+	);
 };
 
-const useFormState = id => {
-    const formState = React.useContext(FormStateContext);
+const useFormState = (id) => {
+	const formState = React.useContext(FormStateContext);
 
-    if (formState === undefined) {
-        throw new Error('useFormState must be used within a FormProvider');
-    }
+	if (formState === undefined) {
+		throw new Error('useFormState must be used within a FormProvider');
+	}
 
-    return formState[id];
+	return formState[id];
 };
 
 const useFormDispatch = () => {
-    const dispatch = React.useContext(FormDispatchContext);
+	const dispatch = React.useContext(FormDispatchContext);
 
-    if (dispatch === undefined) {
-        throw new Error('useFormState must be used within a FormProvider');
-    }
+	if (dispatch === undefined) {
+		throw new Error('useFormState must be used within a FormProvider');
+	}
 
-    return dispatch;
+	return dispatch;
 };
 
 export { FormProvider, useFormState, useFormDispatch };
--- navigation/index.tsx
+++ navigation/index.tsx
@@ -3,60 +3,60 @@
  * https://reactnavigation.org/docs/getting-started
  *
  */
-import { Feather } from "@expo/vector-icons";
-import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
-import { NavigationContainer } from "@react-navigation/native";
-import { createNativeStackNavigator } from "@react-navigation/native-stack";
-import * as React from "react";
-import { ColorSchemeName } from "react-native";
-import HomeScreen from "../screens/HomeScreen/HomeScreen";
-import ClaimScreen from "../screens/ClaimScreen/ClaimScreen";
-import MessageScreen from "../screens/MessageScreen/MessageScreen";
-import SplashScreen from "../screens/SplashScreen/SplashScreen";
-import AgeScreen from "../screens/AgeScreen/AgeScreen";
-import ProfilePicScreen from "../screens/ProfilePicScreen/ProfilePicScreen";
-import UserNameScreen from "../screens/UsernameScreen/UsernameScreen";
-import EmailScreen from "../screens/EmailScreen/EmailScreen";
-import PasswordScreen from "../screens/PasswordScreen/PasswordScreen";
-import WelcomeToPopn from "../screens/WelcomeToPopn/WelcomeToPopn";
-import SignInScreen from "../screens/SignInScreen/SignInScreen";
-import ForgotPasswordScreen from "../screens/ForgotPasswordScreen/ForgotPasswordScreen";
-import SettingsScreen from "../screens/SettingsScreen/SettingsScreen";
-import UserProfileScreen from "../screens/UserProfileScreen/UserProfileScreen";
-import ChangeEmailScreen from "../screens/ChangeEmailScreen/ChangeEmailScreen";
-import EmailVerificationCodeScreen from "../screens/EmailVerificationCodeScreen/EmailVerificationCodeScreen";
-import ChangePasswordScreen from "../screens/ChangePasswordScreen/ChangePasswordScreen";
-import MessageRoomScreen from "../screens/MessageRoomScreen/MessageRoomScreen";
-import NewMessageRoomScreen from "../screens/NewMessageRoomScreen/NewMessageRoomScreen";
+import { Feather } from '@expo/vector-icons';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import * as React from 'react';
+import { ColorSchemeName } from 'react-native';
+import HomeScreen from '../screens/HomeScreen/HomeScreen';
+import ClaimScreen from '../screens/ClaimScreen/ClaimScreen';
+import MessageScreen from '../screens/MessageScreen/MessageScreen';
+import SplashScreen from '../screens/SplashScreen/SplashScreen';
+import AgeScreen from '../screens/AgeScreen/AgeScreen';
+import ProfilePicScreen from '../screens/ProfilePicScreen/ProfilePicScreen';
+import UserNameScreen from '../screens/UsernameScreen/UsernameScreen';
+import EmailScreen from '../screens/EmailScreen/EmailScreen';
+import PasswordScreen from '../screens/PasswordScreen/PasswordScreen';
+import WelcomeToPopn from '../screens/WelcomeToPopn/WelcomeToPopn';
+import SignInScreen from '../screens/SignInScreen/SignInScreen';
+import ForgotPasswordScreen from '../screens/ForgotPasswordScreen/ForgotPasswordScreen';
+import SettingsScreen from '../screens/SettingsScreen/SettingsScreen';
+import UserProfileScreen from '../screens/UserProfileScreen/UserProfileScreen';
+import ChangeEmailScreen from '../screens/ChangeEmailScreen/ChangeEmailScreen';
+import EmailVerificationCodeScreen from '../screens/EmailVerificationCodeScreen/EmailVerificationCodeScreen';
+import ChangePasswordScreen from '../screens/ChangePasswordScreen/ChangePasswordScreen';
+import MessageRoomScreen from '../screens/MessageRoomScreen/MessageRoomScreen';
+import NewMessageRoomScreen from '../screens/NewMessageRoomScreen/NewMessageRoomScreen';
 
 import {
-  RootStackParamList,
-  RootTabParamList,
-  RootTabScreenProps,
-  SettingsStackParamList,
-} from "../types";
-import ResetPasswordScreen from "../screens/ResetPasswordScreen/ResetPasswordScreen";
-import { useAuth } from "../contexts/auth";
-import ShoeDetailsScreen from "../screens/ShoeDetailsScreen/ShoeDetailsScreen";
-import ReferenceScreen from "../screens/ReferenceScreen/ReferenceScreen";
-import NewPostScreen from "../screens/NewPostScreen/NewPostScreen";
-import PostDetailsScreen from "../screens/PostDetailsScreen/PostDetailsScreen";
-import MessageContactsScreen from "../screens/MessageContactsScreen/MessageContactsScreen";
-import UserSearchScreen from "../screens/SearchUserScreen/SearchUserScreen";
+	RootStackParamList,
+	RootTabParamList,
+	RootTabScreenProps,
+	SettingsStackParamList,
+} from '../types';
+import ResetPasswordScreen from '../screens/ResetPasswordScreen/ResetPasswordScreen';
+import { useAuth } from '../contexts/auth';
+import ShoeDetailsScreen from '../screens/ShoeDetailsScreen/ShoeDetailsScreen';
+import ReferenceScreen from '../screens/ReferenceScreen/ReferenceScreen';
+import NewPostScreen from '../screens/NewPostScreen/NewPostScreen';
+import PostDetailsScreen from '../screens/PostDetailsScreen/PostDetailsScreen';
+import MessageContactsScreen from '../screens/MessageContactsScreen/MessageContactsScreen';
+import UserSearchScreen from '../screens/SearchUserScreen/SearchUserScreen';
 
 export default function Navigation({
-  colorScheme,
+	colorScheme,
 }: {
-  colorScheme: ColorSchemeName;
+	colorScheme: ColorSchemeName;
 }) {
-  return (
-    <NavigationContainer
-    // theme={colorScheme === "dark" ? DarkTheme : DefaultTheme}
-    >
-      {/* <OnboardingNavigator /> */}
-      <RootNavigator />
-    </NavigationContainer>
-  );
+	return (
+		<NavigationContainer
+		// theme={colorScheme === "dark" ? DarkTheme : DefaultTheme}
+		>
+			{/* <OnboardingNavigator /> */}
+			<RootNavigator />
+		</NavigationContainer>
+	);
 }
 
 const { Navigator, Screen } = createNativeStackNavigator();
@@ -64,82 +64,82 @@ const { Navigator, Screen } = createNativeStackNavigator();
 const onboardingStack = createNativeStackNavigator<RootStackParamList>();
 
 function OnboardingNavigator() {
-  return (
-    <onboardingStack.Navigator initialRouteName="Splash">
-      <onboardingStack.Screen
-        name="Splash"
-        component={SplashScreen}
-        options={{ headerShown: false }}
-      />
-      <onboardingStack.Screen
-        name="Age"
-        component={AgeScreen}
-        options={{ headerShown: false }}
-      />
-      <onboardingStack.Screen
-        name="ProfilePicture"
-        component={ProfilePicScreen}
-        options={{ headerShown: false }}
-      />
-      <onboardingStack.Screen
-        name="Username"
-        component={UserNameScreen}
-        options={{ headerShown: false }}
-      />
-      <onboardingStack.Screen
-        name="Email"
-        component={EmailScreen}
-        options={{ headerShown: false }}
-      />
-      <onboardingStack.Screen
-        name="Password"
-        component={PasswordScreen}
-        options={{ headerShown: false }}
-      />
-      <onboardingStack.Screen
-        name="Welcome"
-        component={WelcomeToPopn}
-        options={{ headerShown: false }}
-      />
-      <onboardingStack.Screen
-        name="SignIn"
-        component={SignInScreen}
-        options={{ headerShown: false }}
-      />
-      <onboardingStack.Screen
-        name="ForgotPassword"
-        component={ForgotPasswordScreen}
-        options={{ headerShown: false }}
-      />
-      <onboardingStack.Screen
-        name="ResetPassword"
-        component={ResetPasswordScreen}
-        options={{ headerShown: false }}
-      />
-    </onboardingStack.Navigator>
-  );
+	return (
+		<onboardingStack.Navigator initialRouteName="Splash">
+			<onboardingStack.Screen
+				name="Splash"
+				component={SplashScreen}
+				options={{ headerShown: false }}
+			/>
+			<onboardingStack.Screen
+				name="Age"
+				component={AgeScreen}
+				options={{ headerShown: false }}
+			/>
+			<onboardingStack.Screen
+				name="ProfilePicture"
+				component={ProfilePicScreen}
+				options={{ headerShown: false }}
+			/>
+			<onboardingStack.Screen
+				name="Username"
+				component={UserNameScreen}
+				options={{ headerShown: false }}
+			/>
+			<onboardingStack.Screen
+				name="Email"
+				component={EmailScreen}
+				options={{ headerShown: false }}
+			/>
+			<onboardingStack.Screen
+				name="Password"
+				component={PasswordScreen}
+				options={{ headerShown: false }}
+			/>
+			<onboardingStack.Screen
+				name="Welcome"
+				component={WelcomeToPopn}
+				options={{ headerShown: false }}
+			/>
+			<onboardingStack.Screen
+				name="SignIn"
+				component={SignInScreen}
+				options={{ headerShown: false }}
+			/>
+			<onboardingStack.Screen
+				name="ForgotPassword"
+				component={ForgotPasswordScreen}
+				options={{ headerShown: false }}
+			/>
+			<onboardingStack.Screen
+				name="ResetPassword"
+				component={ResetPasswordScreen}
+				options={{ headerShown: false }}
+			/>
+		</onboardingStack.Navigator>
+	);
 }
 
 const settingStack = createNativeStackNavigator<SettingsStackParamList>();
 
 const SettingsNavigator = () => (
-  <settingStack.Navigator
-    screenOptions={{
-      headerShown: false,
-    }}
-    initialRouteName="settings"
-  >
-    <settingStack.Screen name="settings" component={SettingsScreen} />
-    <settingStack.Screen name="changeEmail" component={ChangeEmailScreen} />
-    <settingStack.Screen
-      name="verifyEmail"
-      component={EmailVerificationCodeScreen}
-    />
-    <settingStack.Screen
-      name="changePassword"
-      component={ChangePasswordScreen}
-    />
-  </settingStack.Navigator>
+	<settingStack.Navigator
+		screenOptions={{
+			headerShown: false,
+		}}
+		initialRouteName="settings"
+	>
+		<settingStack.Screen name="settings" component={SettingsScreen} />
+		<settingStack.Screen name="changeEmail" component={ChangeEmailScreen} />
+		<settingStack.Screen
+			name="verifyEmail"
+			component={EmailVerificationCodeScreen}
+		/>
+		<settingStack.Screen
+			name="changePassword"
+			component={ChangePasswordScreen}
+		/>
+	</settingStack.Navigator>
 );
 
 /**
@@ -149,77 +149,77 @@ const SettingsNavigator = () => (
 const Stack = createNativeStackNavigator<RootStackParamList>();
 
 function RootNavigator() {
-  const { session } = useAuth();
+	const { authData } = useAuth();
 
-  return (
-    <Stack.Navigator>
-      {session ? (
-        <>
-          <Stack.Screen
-            name="Root"
-            component={BottomTabNavigator}
-            options={{ headerShown: false }}
-          />
-          <Stack.Screen
-            name="MessageContactsScreen"
-            component={MessageContactsScreen}
-            options={{ headerShown: false }}
-          />
-          <Stack.Screen
-            name="Settings"
-            component={SettingsNavigator}
-            options={{ headerShown: false }}
-          />
-          <Stack.Screen
-            name="UserProfile"
-            component={UserProfileScreen}
-            options={{ headerShown: false }}
-          />
-          <Stack.Screen
-            name="ShoeDetails"
-            component={ShoeDetailsScreen}
-            options={{ headerShown: false }}
-          />
-          <Stack.Screen
-            name="Verify"
-            component={ReferenceScreen}
-            options={{ headerShown: false }}
-          />
-          <Stack.Screen
-            name="NewPost"
-            component={NewPostScreen}
-            options={{ headerShown: false }}
-          />
-          <Stack.Screen
-            name="PostDetails"
-            component={PostDetailsScreen}
-            options={{ headerShown: false }}
-          />
-          <Stack.Screen
-            name="MessageRoom"
-            component={MessageRoomScreen}
-            options={{ headerShown: false }}
-          />
-          <Stack.Screen
-            name="NewMessageRoom"
-            component={NewMessageRoomScreen}
-            options={{ headerShown: false }}
-          />
-          <Stack.Screen
-            name="UserSearch"
-            component={UserSearchScreen}
-            options={{ headerShown: false }}
-          />
-        </>
-      ) : (
-        <Stack.Screen
-          name="Root"
-          component={OnboardingNavigator}
-          options={{ headerShown: false }}
-        />
-      )}
-    </Stack.Navigator>
-  );
+	return (
+		<Stack.Navigator>
+			{authData ? (
+				<>
+					<Stack.Screen
+						name="Root"
+						component={BottomTabNavigator}
+						options={{ headerShown: false }}
+					/>
+					<Stack.Screen
+						name="MessageContactsScreen"
+						component={MessageContactsScreen}
+						options={{ headerShown: false }}
+					/>
+					<Stack.Screen
+						name="Settings"
+						component={SettingsNavigator}
+						options={{ headerShown: false }}
+					/>
+					<Stack.Screen
+						name="UserProfile"
+						component={UserProfileScreen}
+						options={{ headerShown: false }}
+					/>
+					<Stack.Screen
+						name="ShoeDetails"
+						component={ShoeDetailsScreen}
+						options={{ headerShown: false }}
+					/>
+					<Stack.Screen
+						name="Verify"
+						component={ReferenceScreen}
+						options={{ headerShown: false }}
+					/>
+					<Stack.Screen
+						name="NewPost"
+						component={NewPostScreen}
+						options={{ headerShown: false }}
+					/>
+					<Stack.Screen
+						name="PostDetails"
+						component={PostDetailsScreen}
+						options={{ headerShown: false }}
+					/>
+					<Stack.Screen
+						name="MessageRoom"
+						component={MessageRoomScreen}
+						options={{ headerShown: false }}
+					/>
+					<Stack.Screen
+						name="NewMessageRoom"
+						component={NewMessageRoomScreen}
+						options={{ headerShown: false }}
+					/>
+					<Stack.Screen
+						name="UserSearch"
+						component={UserSearchScreen}
+						options={{ headerShown: false }}
+					/>
+				</>
+			) : (
+				<Stack.Screen
+					name="Root"
+					component={OnboardingNavigator}
+					options={{ headerShown: false }}
+				/>
+			)}
+		</Stack.Navigator>
+	);
 }
 
 /**
@@ -229,50 +229,50 @@ function RootNavigator() {
 const BottomTab = createBottomTabNavigator<RootTabParamList>();
 
 function BottomTabNavigator() {
-  return (
-    <BottomTab.Navigator
-      initialRouteName="Home"
-      screenOptions={{
-        tabBarActiveTintColor: "black",
-        headerShown: false,
-      }}
-    >
-      <BottomTab.Screen
-        name="Home"
-        component={HomeScreen}
-        options={({ navigation }: RootTabScreenProps<"Home">) => ({
-          title: "Home",
-          tabBarIcon: ({ color }) => <TabBarIcon name="home" color={color} />,
-        })}
-      />
-      <BottomTab.Screen
-        name="Claim"
-        component={ClaimScreen}
-        options={{
-          title: "Claim",
-          tabBarIcon: ({ color }) => <TabBarIcon name="search" color={color} />,
-        }}
-      />
-      <BottomTab.Screen
-        name="Message"
-        component={MessageScreen}
-        options={{
-          title: "Messages",
-          tabBarIcon: ({ color }) => (
-            <TabBarIcon name="message-square" color={color} />
-          ),
-        }}
-      />
-    </BottomTab.Navigator>
-  );
+	return (
+		<BottomTab.Navigator
+			initialRouteName="Home"
+			screenOptions={{
+				tabBarActiveTintColor: 'black',
+				headerShown: false,
+			}}
+		>
+			<BottomTab.Screen
+				name="Home"
+				component={HomeScreen}
+				options={({ navigation }: RootTabScreenProps<'Home'>) => ({
+					title: 'Home',
+					tabBarIcon: ({ color }) => <TabBarIcon name="home" color={color} />,
+				})}
+			/>
+			<BottomTab.Screen
+				name="Claim"
+				component={ClaimScreen}
+				options={{
+					title: 'Claim',
+					tabBarIcon: ({ color }) => <TabBarIcon name="search" color={color} />,
+				}}
+			/>
+			<BottomTab.Screen
+				name="Message"
+				component={MessageScreen}
+				options={{
+					title: 'Messages',
+					tabBarIcon: ({ color }) => (
+						<TabBarIcon name="message-square" color={color} />
+					),
+				}}
+			/>
+		</BottomTab.Navigator>
+	);
 }
 
 /**
  * You can explore the built-in icon families and icons on the web at https://icons.expo.fyi/
  */
 function TabBarIcon(props: {
-  name: React.ComponentProps<typeof Feather>["name"];
-  color: string;
+	name: React.ComponentProps<typeof Feather>['name'];
+	color: string;
 }) {
-  return <Feather size={20} style={{ marginBottom: -3 }} {...props} />;
+	return <Feather size={20} style={{ marginBottom: -3 }} {...props} />;
 }
