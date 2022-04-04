f97d52bf04b382bbe390c42eb5b20ea95f96f338
Anthony Barrera <47405290+AnthonyCardenBarrera@users.noreply.github.com>
2022-04-04 09:59:22 -0400

    create listing screen styled

--- screens/NewListingScreen/index.tsx
+++ screens/NewListingScreen/index.tsx
@@ -9,11 +9,13 @@ import {
 	AutoImage as Image,
 	TextField,
 } from '../../components';
-
+import { FontAwesome } from '@expo/vector-icons';
 import styles from './styles';
 
 interface NewListingProps { }
 const NewListingScreen: FC<NewListingProps> = () => {
+	const [price, setPrice] = React.useState('');
+	const [description, setDescription] = React.useState('');
 	const navigation = useNavigation();
 	return (
 		<Screen style={styles.CONTAINER}>
@@ -31,27 +33,23 @@ const NewListingScreen: FC<NewListingProps> = () => {
 				{/* SECTION: for title input */}
 				<Text text="Title" preset='bold' />
 				<View style={styles.INPUT_FIELDS_CONTAINER}>
-					<TextInput
-						style={styles.TEXTFIELD_STYLE}
-						// value={query}
-						autoCorrect={false}
-						// onChangeText={(text) => setQuery(text)}
-						placeholder="Search"
-						placeholderTextColor={'#878C90'}
-					/>
+					<Text preset='default'>Jordan 11 Retro Cool Gray</Text>
 				</View>
+
 				{/* SECTION: for price input */}
 				<Text text="Price" preset='bold' style={{ marginTop: 6 }} />
 				<View style={styles.INPUT_FIELDS_CONTAINER}>
+					<FontAwesome name="dollar" size={20} color="black" />
 					<TextInput
 						style={styles.TEXTFIELD_STYLE}
-						// value={query}
+						value={price}
 						autoCorrect={false}
-						// onChangeText={(text) => setQuery(text)}
-						placeholder="Search"
+						onChangeText={(text) => setPrice(text)}
+						placeholder="0"
 						placeholderTextColor={'#878C90'}
 					/>
 				</View>
+
 				{/* SECTION: for condition selection */}
 				<Text text="Condition" preset='bold' style={{ marginTop: 6 }} />
 				<View style={styles.INPUT_FIELDS_CONTAINER}>
@@ -64,6 +62,7 @@ const NewListingScreen: FC<NewListingProps> = () => {
 						placeholderTextColor={'#878C90'}
 					/>
 				</View>
+
 				{/* SECTION: for shoe size selection */}
 				<Text text="Size" preset='bold' style={{ marginTop: 6 }} />
 				<View style={styles.INPUT_FIELDS_CONTAINER}>
@@ -79,19 +78,32 @@ const NewListingScreen: FC<NewListingProps> = () => {
 
 				{/* SECTION: for inputing the descriptions */}
 				<Text text="Description" preset='bold' style={{ marginTop: 6 }} />
-				<View style={styles.INPUT_FIELDS_CONTAINER}>
+				<View style={styles.MESSAGE_BOX}>
 					<TextInput
-						style={styles.TEXTFIELD_STYLE}
-						// value={query}
+						style={{
+							flex: 1,
+							width: '10%',
+							height: '100%',
+							borderWidth: 1,
+							paddingLeft: 10,
+							borderRadius: 5,
+							borderColor: '#FFFFFF',
+							backgroundColor: 'white',
+						}}
+						value={description}
 						autoCorrect={false}
-						// onChangeText={(text) => setQuery(text)}
-						placeholder="Search"
+						onChangeText={(text) => setDescription(text)}
+						placeholder=""
 						placeholderTextColor={'#878C90'}
+						keyboardType="default"
+						multiline={true}
+						blurOnSubmit={true}
+						returnKeyType="done"
 					/>
 				</View>
 
 				{/* SECTION: for importing images */}
-				<Text text="Add Photos" preset='bold' style={{ marginTop: 6 }} />
+				{/* <Text text="Add Photos" preset='bold' style={{ marginTop: 6 }} />
 				<View style={styles.INPUT_FIELDS_CONTAINER}>
 					<TextInput
 						style={styles.TEXTFIELD_STYLE}
@@ -101,9 +113,9 @@ const NewListingScreen: FC<NewListingProps> = () => {
 						placeholder="Search"
 						placeholderTextColor={'#878C90'}
 					/>
-					{/* <Text>{'Make sure you include the following photos'}</Text> */}
+					<Text>{'Make sure you include the following photos'}</Text>
 				</View>
-				<Text>{'Make sure you include the following photos'}</Text>
+				<Text>{'Make sure you include the following photos'}</Text> */}
 			</View>
 		</Screen>
 	);
