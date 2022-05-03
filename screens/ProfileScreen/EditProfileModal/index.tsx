b4d3ec243c1cf2a1c71131b2c277205b5e7eb4a0
Anthony Barrera <47405290+AnthonyCardenBarrera@users.noreply.github.com>
2022-05-03 12:51:08 -0400

    switch media type of image picker to only images

--- screens/ProfileScreen/EditProfileModal/index.tsx
+++ screens/ProfileScreen/EditProfileModal/index.tsx
@@ -49,7 +49,7 @@ const EditProfileModal: React.FunctionComponent<EditProfileModalProps> = ({ edit
     //allow user to pick an image from library
     const pickImage = async () => {
         let result = await ImagePicker.launchImageLibraryAsync({
-            mediaTypes: ImagePicker.MediaTypeOptions.All,
+            mediaTypes: ImagePicker.MediaTypeOptions.Images,
             allowsEditing: true,
             aspect: [4, 3],
             quality: 1,
